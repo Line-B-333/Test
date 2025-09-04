14596ad329dd0ab577d4cff278362afee6e64955
Line-B-333 <10187764@ucn.dk>
2025-09-04 12:46:59 +0200

    Uploading: Testing.CkassLibrary and Testing.ConsoleApp

---
+++ Testing.ClassLibrary/Kunde.cs
@@ -0,0 +1,57 @@
+﻿namespace Testing.ClassLibrary;
+
+//Jacob "Hvorfor virker klassen ikke" - Svar: "Der mangler en main metode"
+public class Kunde
+{
+
+    public Kunde(string Name, int Age, long Cpr)
+    {
+        this.Name = Name;
+        this.Age = Age;
+        this.Cpr = Cpr;
+    }
+    public string Name { get; set; }
+
+    //Der er en skjult instance variable ligger i back end field
+    public int Age { get; set; }
+
+    //Do not use int for CPR, cause 0 disappears
+    public long Cpr { get; private set; }
+
+    /// <summary>
+    /// A method that takes any number of names and returns them as a single string, separated by commas.
+    /// 
+    /// By makeing it static it's doesnt belong to an instance of kunde
+    /// Which means the method can be used without creating an instance of the Kunde class.
+    /// </summary>
+    /// <param name="names"></param>
+    /// <returns></returns>
+    public static string AddAnyNumberOfNames(params string[] names)
+    {
+        return string.Join(", ", names);
+    }
+
+    public static void GreetCustomer(params string[] names)
+    {
+        if (names.Length == 0)
+        {
+            Console.WriteLine("Usage: Testing.ConsoleApp.exe <name1> <name2> ... <nameN>");
+        }
+        else
+        {
+            foreach (string name in names)
+            {
+                if (name == "Line")
+                {
+                    Console.WriteLine($"Welcome back {name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello, {name}!");
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Testing.ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Testing.ConsoleApp;
+using Testing.ClassLibrary;
+internal class Program
+{
+      
+    static void Main(string[] args)
+    {
+        // 12. Using the static method GreetCustomer to greet any number of customers passed as command line arguments
+        // This calls the method GreetCustomer and pass the args along
+        // Args is an array of strings, which can hold any number of names
+        Kunde.GreetCustomer(args);
+
+        //9. Second attempt on making a method to add any number of names and greeting each person
+        /**
+         
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: Testing.ConsoleApp.exe <name1> <name2> ... <nameN>");
+        }
+        else
+        {
+            foreach (string name in args)
+            {
+                //10. Says welcome back to Line
+                if (name == "Line")
+                {
+                    Console.WriteLine($"Welcome back {name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello, {name}!");
+                }
+                    
+            }
+        }*/
+
+        //9. First attempt on making a method to add any number of names and greeting each person
+        //String printKundeNavne = Kunde.AddAnyNumberOfNames("Lars", "Peter", "Hans", "Kurt", "Finn", "Jan", "Bo", "Kim");
+        //Console.WriteLine(printKundeNavne);
+
+        /**         
+        if (args.Length >= 2)
+        {
+            string name = args[0];
+            int age = int.Parse(args[1]);
+            Console.WriteLine($"Hello {name}, you are {age} years old.");
+            Kunde kunde = new Kunde(name, age, 1234567890);
+            Console.WriteLine($"Kunde created: Name = {kunde.Navn}, Age = {kunde.Age}, CPR = {kunde.Cpr}");
+        }
+        else
+        {
+            Console.WriteLine("Please provide at least two arguments: name and age.");
+        }*/
+
+
+
+
+        //Console.WriteLine("Please enter name: ");
+        //string? name = Console.ReadLine();
+        //Console.WriteLine($"Hello {name}, and welcome to c#");
+        //Kunde kunde = new Kunde("Lars", 30, 123456-7890);
+
+
+    }
+}
